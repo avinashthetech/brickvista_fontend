6438a9a1af74336551e40237ea907d1511d60895
avinashthetech <avinash2019psit@gmail.com>
2026-03-29 05:15:04 -0800

    add this game

--- src/HowItWorks.jsx
+++ src/HowItWorks.jsx
@@ -1,35 +1,43 @@
 import "./HowItWorks.css";
 
+
+import img1 from "../assets/images/timeline-pic-1.webp";
+import img2 from "../assets/images/timeline-pic-2.webp";
+import img3 from "../assets/images/timeline-pic3.webp";
+import img4 from "../assets/images/timeline-pic4.webp";
+import img5 from "../assets/images/timeline-pic6.webp";
+
+
 const steps = [
   {
     id: "01",
     title: "Technical Consultation",
     desc: "Discuss your requirements and vision with our experienced architects and technical experts to shape a clear project roadmap.",
-    img: "../src/assets/images/timeline-pic-1.webp",
+    img: img1,
   },
   {
     id: "02",
     title: "Booking",
     desc: "Secure your project by paying a nominal booking amount and officially initiate the construction process.",
-    img: "../src/assets/images/timeline-pic-2.webp",
+    img: img2,
   },
   {
     id: "03",
     title: "Design & Planning",
     desc: "Our architects prepare detailed designs, layouts, and cost plans tailored to your needs and budget.",
-    img: "../src/assets/images/timeline-pic3.webp",
+    img: img3,
   },
   {
     id: "04",
     title: "Construction",
     desc: "Construction begins using quality materials, expert supervision, and regular progress updates at every stage.",
-    img: "../src/assets/images/timeline-pic4.webp",
+    img: img4,
   },
   {
     id: "05",
     title: "Handover & Move-In",
     desc: "Final inspection, quality checks, and smooth handover so you can move into your home with confidence and warranty.",
-    img: "../src/assets/images/timeline-pic6.webp",
+    img: img5,
   },
 ];
 
